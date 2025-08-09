d4f237d1a84dabdab8022b869d84ef4b10515b1a
Melbin J Paulose <91303803+melbinjp@users.noreply.github.com>
2025-08-09 18:08:54 +0530

    intial commit2

---
+++ config.js
@@ -0,0 +1,10 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAlfXMGejp35bsiU74h9IaoHt5HgLZz08E",
+  authDomain: "phone-wecanuseai.firebaseapp.com",
+  databaseURL: "https://phone-wecanuseai-default-rtdb.firebaseio.com",
+  projectId: "phone-wecanuseai",
+  storageBucket: "phone-wecanuseai.firebasestorage.app",
+  messagingSenderId: "1003049877741",
+  appId: "1:1003049877741:web:0044e7bb1ebf366f918ce4"
+};
